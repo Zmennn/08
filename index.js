8841ba8716762c23ded07986231eeb3200e156a6
Zmennn <79196210+Zmennn@users.noreply.github.com>
2021-07-14 10:26:42 +0300

    0.5

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 "use strict"
 // камень-ножницы-бумага
-// let go = 0;
-let comp = 0;
-let play = 0;
-let a = 0;
+
 let playScore = 0;
 let compScore = 0;
 let id = 0;
@@ -16,35 +13,14 @@ const refs = {
     rockEl: document.querySelector('.rock-container'),
     paperEl: document.querySelector('.paper-container'),
     scissorsEl: document.querySelector('.scissors-container'),
+    resultContainerEl: document.querySelector('.result-container'),
+    playScoreEl: document.querySelector('.player-score'),
+    compScoreEl: document.querySelector('.comp-score'),
 }
 
 initialData()
 
-//Основное тело -бесконечный цикл прерываемый по команде от игрока
-// do {
-//     initialData();
 
-//     if (a === null) {
-//         break;
-//     };
-
-//     const resultText = (dataProcessing());
-
-//     resultPrint.call(resultText);
-
-//     resultsBase(resultText);
-
-
-
-
-// }
-// while (a !== null);
-
-// console.table(dataBase);
-
-
-
-// console.log('приходите исчо, для продолжения можно обновить страницу,счет будет обнулен');
 
 
 //Выбор игрока, включение иконок
@@ -53,115 +29,97 @@ function initialData() {
     refs.rockEl.classList.remove('is-hidden');
     refs.paperEl.classList.remove('is-hidden');
     refs.scissorsEl.classList.remove('is-hidden');
-    refs.rockEl.addEventListener('click', event => compChoice('rock'));
-    refs.paperEl.addEventListener('click', event => compChoice('paper'));
-    refs.scissorsEl.addEventListener('click', event => compChoice('scissors'));
+    refs.rockEl.addEventListener('click', event => compChoiceAdd('rock'));
+    refs.paperEl.addEventListener('click', event => compChoiceAdd('paper'));
+    refs.scissorsEl.addEventListener('click', event => compChoiceAdd('scissors'));
 };
 
 
-
-function compChoice(playerChoice) {
+//Создаем выбор компа и передаем в обработку вместе с выбором игрока
+function compChoiceAdd(playerChoice) {
     const comp = Math.ceil(Math.random() * 3);
-    let compItem = '';
+    let compChoice = '';
     switch (comp) {
         case (1):
-            compItem = 'rock'
+            compChoice = 'rock'
             break;
         case (2):
-            compItem = 'paper'
+            compChoice = 'paper'
             break;
         case (3):
-            compItem = 'scissors'
+            compChoice = 'scissors'
             break;
     };
-    dataProcessing(playerChoice, compItem)
+    dataProcessing(playerChoice, compChoice)
 }
 
 
-//Обработка данных и выдача массива тектстовых данных результатов
-function dataProcessing(playerChoice, compItem) {
-    console.log(playerChoice, compItem);
-    let playItem = '';
-    // let compItem = '';
+//Обработка данных и выдача массива  данных результатов
+function dataProcessing(playerChoice, compChoice) {
+    console.log(playerChoice, compChoice);
+
     let message = '';
 
-
-    if (play === 1 && comp === 1) {
-        message = 'Hичья';
-    }
-
-    else if (play === 1 && comp === 2) {
-        message = 'Вы победили';
-        playScore += 1;
-    }
-    else if (play === 1 && comp === 3) {
-        message = 'Вы проиграли';
-        compScore += 1;
-    }
-    else if (play === 2 && comp === 1) {
-        message = 'Вы проиграли';
-        compScore += 1;
-    }
-    else if (play === 2 && comp === 2) {
-        message = 'Hичья';
-
-    }
-    else if (play === 2 && comp === 3) {
-        message = 'Вы победили';
-        playScore += 1;
-    }
-    else if (play === 3 && comp === 1) {
-        message = 'Вы победили';
-        playScore += 1;
-    }
-    else if (play === 3 && comp === 2) {
-        message = 'Вы проиграли';
-        compScore += 1;
-    }
-    else if (play === 3 && comp === 3) {
-        message = 'Hичья';
+    const innerData = {
+        rockrock: 'draw',
+        rockpaper: 'loss',
+        rockscissors: 'win',
+        paperpaper: 'draw',
+        paperrock: 'win',
+        paperscissors: 'loss',
+        scissorsscissors: 'draw',
+        scissorsrock: 'loss',
+        scissorspaper: 'win'
     };
 
-    switch (play) {
-        case (1):
-            playItem = 'камень'
-            break;
-        case (2):
-            playItem = 'ножницы'
-            break;
-        case (3):
-            playItem = 'бумага'
-            break;
+    message = innerData[playerChoice + compChoice];
 
-        default:
-            break;
-    };
-    switch (comp) {
-        case (1):
-            compItem = 'камень'
-            break;
-        case (2):
-            compItem = 'ножницы'
-            break;
-        case (3):
-            compItem = 'бумага'
-            break;
+    console.log(message);
 
-        default:
+    switch (message) {
+        case 'win':
+            playScore += 1;
             break;
-    };
+        case 'loss':
+            compScore += 1;
+            break;
+    }
 
     const result = {
-        playItem,
-        compItem,
+        playerChoice,
+        compChoice,
         message,
         playScore,
         compScore,
     };
 
-    return result;
+    resultsBase(result);
+    showResult(result);
 };
 
+//Отображает результаты последнего матча на экране
+function showResult(result) {
+    refs.rockEl.classList.add('is-hidden');
+    refs.paperEl.classList.add('is-hidden');
+    refs.scissorsEl.classList.add('is-hidden');
+    refs.resultContainerEl.classList.remove('is-hidden');
+    refs.resultContainerEl.style.backgroundSize = 'cover';
+
+    switch (result.message) {
+        case 'win':
+            refs.resultContainerEl.style.backgroundImage = 'url(./images/win.jpg)';
+            break;
+        case 'loss':
+            refs.resultContainerEl.style.backgroundImage = 'url(./images/loss.png)';
+            break;
+        case 'draw':
+            refs.resultContainerEl.style.backgroundImage = 'url(./images/draw.png)';
+            break;
+    };
+
+    refs.playScoreEl.textContent = result.playScore;
+    refs.compScoreEl.textContent = result.compScore;
+}
 
 // формит массив
 function resultsBase(innerArray) {
@@ -172,14 +130,14 @@ function resultsBase(innerArray) {
 
 };
 
-//выводит результат каждого тура
-function resultPrint() {
-    console.log(`${this.message}
+// //выводит результат каждого тура
+// function resultPrint() {
+//     console.log(`${this.message}
 
-    вы выбрали-${this.playItem}   компьютер выбрал-${this.compItem}
+//     вы выбрали-${this.playItem}   компьютер выбрал-${this.compItem}
 
-    общий счет-игрок${this.playScore} комп${this.compScore}`);
-};
+//     общий счет-игрок${this.playScore} комп${this.compScore}`);
+// };
 
 
 //будет выводить таблицу результатов после игры
